b0e601603233294c4588193af4a6447aafc1c063
neo <Maxim1106@mail.ru>
2019-10-06 23:24:57 +0300

    added page 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -50,47 +50,64 @@ export class AppComponent {
       animated: true
     });
   }
-//
-//   slides = [
-//     {img: '../assets/img/slider/slider-1/1.jpg'},
-//     {img: '../assets/img/slider/slider-1/2.jpg'},
-//     {img: '../assets/img/slider/slider-1/3.jpg'},
-//     {img: '../assets/img/slider/slider-1/4.jpg'},
-//     {img: '../assets/img/slider/slider-1/5.jpg'}
-//   ];
-//
-//   slideConfig = {
-//     'slidesToShow': 4,
-//     'slidesToScroll': 1,
-//     'nextArrow':'<div class="nav-btn next-slide"></div>',
-//     'prevArrow':'<div class="nav-btn prev-slide"></div>',
-//     'dots':true,
-//     'infinite': false
-//   };
-//
-//   addSlide() {
-//     this.slides.push({img: 'http://placehold.it/350x150/777777'});
-//   }
-//
-//   removeSlide() {
-//     this.slides.length = this.slides.length - 1;
-//   }
-//
-//   slickInit(e) {
-//     console.log('slick initialized');
-//   }
-//
-//   breakpoint(e) {
-//     console.log('breakpoint');
-//   }
-//
-//   afterChange(e) {
-//     console.log('afterChange');
-//   }
-//
-//   beforeChange(e) {
-//     console.log('beforeChange');
-//   }
-//
-//
+
+  altitudeSlides = [
+    {img: '../assets/img/slider/slider-1/1.jpg'},
+    {img: '../assets/img/slider/slider-1/2.jpg'},
+    {img: '../assets/img/slider/slider-1/3.jpg'},
+    {img: '../assets/img/slider/slider-1/4.jpg'},
+    {img: '../assets/img/slider/slider-1/5.jpg'}
+  ];
+
+  installationSlides = [
+    {img: '../assets/img/slider/slider-2/1.jpg'},
+    {img: '../assets/img/slider/slider-2/2.jpg'},
+    {img: '../assets/img/slider/slider-2/3.jpg'},
+    {img: '../assets/img/slider/slider-2/4.jpg'},
+    {img: '../assets/img/slider/slider-2/5.jpg'},
+    {img: '../assets/img/slider/slider-2/6.jpg'},
+    {img: '../assets/img/slider/slider-2/7.jpg'},
+    {img: '../assets/img/slider/slider-2/8.jpg'},
+    {img: '../assets/img/slider/slider-2/9.jpg'}
+  ];
+
+  frontSlides = [
+    {img: '../assets/img/slider/slider-3/1.jpg'},
+    {img: '../assets/img/slider/slider-3/2.jpg'},
+    {img: '../assets/img/slider/slider-3/3.jpg'},
+    {img: '../assets/img/slider/slider-3/4.jpg'},
+    {img: '../assets/img/slider/slider-3/5.jpg'},
+    {img: '../assets/img/slider/slider-3/6.jpg'}
+  ];
+
+  roofingSlides = [
+    {img: '../assets/img/slider/slider-4/1.jpg'}
+  ];
+
+  slideConfig = {
+    'slidesToShow': 6,
+    'slidesToScroll': 1,
+    'nextArrow':'<div class="nav-btn next-slide"></div>',
+    'prevArrow':'<div class="nav-btn prev-slide"></div>',
+    'dots':true,
+    'infinite': false
+  };
+
+  slickInit(e) {
+    console.log('slick initialized');
+  }
+
+  breakpoint(e) {
+    console.log('breakpoint');
+  }
+
+  afterChange(e) {
+    console.log('afterChange');
+  }
+
+  beforeChange(e) {
+    console.log('beforeChange');
+  }
+
+
 }
